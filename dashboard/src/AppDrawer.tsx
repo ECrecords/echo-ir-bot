3a0ccde02b91ce3a22d0024cd1d60d66ceb3d2dd
Elvis Chino-Islas <echinoislas+github@gmail.com>
2024-05-07 22:25:34 -0700

    dashboard now displays data

--- dashboard/src/AppDrawer.tsx
+++ dashboard/src/AppDrawer.tsx
@@ -1,90 +1,132 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+
+import SettingIcon from '@mui/icons-material/Settings';
+import DataIcon from '@mui/icons-material/ShowChart';
 import RadarIcon from '@mui/icons-material/Radar';
-import ShowChartIcon from '@mui/icons-material/ShowChart';
+import { Button } from '@mui/material';
 
-import RadarDisplay from './RadarDisplay';
-import LineChartDisplay from './LineChartDisplay';
 
-// Configuration.js
-function Configuration() {
-    return <p>This is the Configuration component.</p>;
-}
-  
+import SimpleLineChart from './DisplayData';
 
 const drawerWidth = 240;
 
-export default function AppDrawer() {
-  const [selectedComponent, setSelectedComponent] = React.useState('Configuration');
+interface Data {
+  distance: Array<number>;
+  angle: Array<number>;
+}
 
-  const handleListItemClick = (component: any) => {
-    setSelectedComponent(component);
-  };
+
+function AppDrawer() {
+
+  // state for slected item
+  const [selectedItem, setSelectedItem] = React.useState('Configurations');
+  const [startDisabled, setStartDisabled] = React.useState(true);
+  const [stopDisabled, setStopDisabled] = React.useState(false);
+  const [samples, setSamples] = React.useState<Data>({
+    distance: [],
+    angle: []
+  });
+
+  React.useEffect( () => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/data')   
+        const updateSamples = await response.json();
+        setSamples(updateSamples);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+
+    const interval = setInterval(fetchData, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+
+
+  const startButtonClickHandler = () => {
+    console.log('start button clicked');
+    setStartDisabled(true);  // Disable the start button
+    setStopDisabled(false);  // Enable the stop button
+  }
+
+  const stopButtonClickHandler = () => {
+    console.log('stop button clicked');
+    setStopDisabled(true);  // Enable the start button
+    setStartDisabled(false)
+  }
 
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      >
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
-            Radar Navagation
+            Clipped drawer
           </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
+        variant="permanent"
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
         }}
-        variant="permanent"
-        anchor="left"
       >
         <Toolbar />
-        <Divider />
-        <List>
-          {['Configuration', 'Data', 'Radar'].map((text) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton onClick={() => handleListItemClick(text)}>
-                <ListItemIcon>
-                  {text === 'Configuration' && <SettingsApplicationsIcon />}
-                  {text === 'Data' && <ShowChartIcon />}
-                  {text === 'Radar' && <RadarIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-      >
-        <Toolbar />
-        {selectedComponent === 'Configuration' && <Configuration />}
-        {selectedComponent === 'Radar' && <RadarDisplay angle={90} distance={100} />}
-        {selectedComponent === 'Data' && <LineChartDisplay />}
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            {['Configurations', 'Data', 'Radar'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton onClick={() => setSelectedItem(text)}>
+                  <ListItemIcon>
+                    {text === 'Configurations' && <SettingIcon />}
+                    {text === 'Data' && <DataIcon />}
+                    {text === 'Radar' && <RadarIcon />}
 
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+
+          <Stack spacing={2} padding={5}>
+              <Button variant="contained" color="success" id='startButton' onClick={startButtonClickHandler} disabled={startDisabled}>
+                START
+              </Button>
+
+              <Button variant="contained" color="error" onClick={stopButtonClickHandler} disabled={stopDisabled}>
+                STOP
+              </Button>
+          </Stack>
+        </Box>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        <SimpleLineChart distance={samples.distance} angle={samples.angle}/>
       </Box>
     </Box>
   );
 }
+
+
+export default AppDrawer;
